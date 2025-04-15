1c189668f349ef489839f6d3ce91e0cbd1f04d49
Gowthams1999-commits <githubgowtham1999@gmail.com>
2025-04-15 11:39:57 +0530

    Create variable.tf

---
+++ Day 6/modules/ec2/variable.tf
@@ -0,0 +1,23 @@
+variable "ec2_ami" {
+
+description = "This is ami for instance"
+
+}
+
+variable "instance_type" {
+
+description = "This is instance type for instance"
+
+}
+
+variable "key_value_pair" {
+
+description = "This is key value pair for instance"
+
+}
+
+variable "tags_name" {
+
+description = "This is tags name"
+
+}
